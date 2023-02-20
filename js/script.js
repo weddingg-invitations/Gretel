29e23c719bfdae6fd4f45ef17ea240c04e4ceee6
Edgar Karapetyan <edo099919956@gmail.com>
2023-02-20 21:48:24 +0400

    new gretel strated

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-// // logo animation 
-// let b = document.querySelectorAll('.logo > b')
-// setInterval(() => {
-//     setTimeout(() => {
-//         b[0].classList.add('anim-add')
-//     }, 0);
-//     setTimeout(() => {
-//         b[1].classList.add('anim-add')
-//     }, 100);
-//     setTimeout(() => {
-//         b[2].classList.add('anim-add')
-//     }, 200);
-//     setTimeout(() => {
-//         b[3].classList.add('anim-add')
-//     }, 300);
-//     setTimeout(() => {
-//         b[4].classList.add('anim-add')
-//     }, 400);
-//     setTimeout(() => {
-//         b[5].classList.add('anim-add')
-//     }, 500);
-
-//     setTimeout(() => {
-//         b.forEach(sp => {
-//             sp.classList.remove('anim-add')
-//         })
-//     }, 3000);
-// }, 3000);
-
 // popup closed
 const popup_loader = document.querySelector('.popup_loader')
 const cssload_preloader = document.querySelector('.cssload-preloader')
