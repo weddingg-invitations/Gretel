8c78478f959c0b48f1a67609b695fe2cae2909b1
Edgar <edo099919956@gmail.com>
2023-03-09 11:13:39 -0800

    timer ready

--- js/script.js
+++ js/script.js
@@ -11,3 +11,66 @@ setTimeout(() => {
 setTimeout(() => {
     popup_loader.style.cssText = 'display: none;'
 }, 5000)
+
+// -------------------------------------------------------------------------
+// timer 
+let hour = document.querySelector('.hour'),
+    min = document.querySelector('.min'),
+    sec = document.querySelector('.sec');
+
+let H = 2, //hour
+    M = 0, //min
+    S = 0; //sec
+
+if (localStorage.getItem) {
+    H = localStorage.getItem('hour', H)
+    M = localStorage.getItem('min', M)
+    S = localStorage.getItem('sec', S)
+
+    let interval = setInterval(() => {
+        timer()
+    }, 1000);
+
+    function timer() {
+        if (H == 0 && M == 0 && S == 0) {
+            clearInterval(interval())
+            H = 3; M = 0; S = 0;
+        }
+        // S
+        if (S < 0) {
+            S = 60
+            M--
+        } else {
+            if (S <= 9) {
+                sec.textContent = `0${S}`
+            } else {
+                sec.textContent = S
+            }
+        }
+
+        // M
+        if (M < 0) {
+            M = 60
+            H--
+        } else {
+            if (M <= 9) {
+                min.textContent = `0${M}`
+            } else {
+                min.textContent = M
+            }
+        }
+
+        // H
+        if (H <= 9) {
+            hour.textContent = `0${H}`
+        } else {
+            hour.textContent = H
+        }
+
+        localStorage.setItem('hour', H)
+        localStorage.setItem('min', M)
+        localStorage.setItem('sec', S)
+
+        S--
+    }
+}
